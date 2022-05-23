5690dca94f2352a0dbdbe95e903f5011170471d9
Branislav Mihajlovic <105886835+25business@users.noreply.github.com>
2022-05-23 14:39:30 +0200

    Add files via upload

---
+++ Kalkulator.java
@@ -0,0 +1,12 @@
+public class Kalkulator {
+	public static void main(String[] args) {
+		
+		double a = 5.5;
+		double b = 7.7;
+		
+		System.out.println("Sabiranje brojeva: 5.5 + 7.7 = " + (a + b));
+		System.out.println("Oduzimanje brojeva: 5.5 - 7.7 = " + (a - b));
+		System.out.println("Mnozenje brojeva: 5.5 * 7.7 = " + (a * b));
+		System.out.println("Deljenje brojeva: 5.5 / 7.7 = " + (a / b));
+	}
+}
---
+++ Konverter.java
@@ -0,0 +1,11 @@
+public class Konverter {
+	public static void main(String[] args) {
+		
+		double dinari = 21000;
+		
+		System.out.println("21000 dinara je " + (dinari / 117.4684) + " evra");
+		System.out.println("21000 dinara je " + (dinari / 113.4522) + " franaka");
+		System.out.println("21000 dinara je " + (dinari / 1.7632) + " rublja");
+		System.out.println("21000 dinara je " + (dinari / 111.8427) + " dolara");
+	}
+}
